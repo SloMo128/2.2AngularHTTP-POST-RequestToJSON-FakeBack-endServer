8dfed2dcbd31ca524d8234f5195f66292054980a
SloMo128 <150801894+SloMo128@users.noreply.github.com>
2024-05-30 22:37:53 +0200

    Add files via upload

---
+++ angular-frontend/src/app/service/api.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Person } from '../model/person';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+ 
+@Injectable({providedIn:'root'})
+export class PersonApiService {
+ 
+  baseURL: string = "http://localhost:3000/";
+ 
+  constructor(private http: HttpClient) {}
+ 
+  getPeople(): Observable<Person[]> {
+    return this.http.get<Person[]>(this.baseURL + 'people');
+  }
+ 
+  addPerson(person:Person): Observable<Person> {
+    const headers = { 'content-type': 'application/json'}  
+    const body=JSON.stringify(person);
+    console.log(body)
+    return this.http.post<Person>(this.baseURL + 'people', body,{'headers':headers})
+  }
+}
