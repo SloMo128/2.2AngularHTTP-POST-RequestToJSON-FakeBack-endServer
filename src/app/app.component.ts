2265fb2a6d8c4327064b9004dfef5ba7d9985354
SloMo128 <nesci.alessandro.17@outlook.it>
2024-05-30 22:44:24 +0200

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PersonApiService } from './service/api.service';
+import { Person } from './model/person';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-frontend';
+export class AppComponent implements OnInit {
+
+  title = 'Angular HTTP POST request to JSON back-end server';
+  people: Person[];
+  person = new Person(null, "");
+
+  constructor(private apiService: PersonApiService) { }
+
+  ngOnInit() {
+    this.refreshPeople()
+  }
+
+  refreshPeople() {
+    this.apiService.getPeople()
+      .subscribe(data => {
+        console.log(data);
+        this.people = data;
+      })
+
+  }
+
+  addPerson() {
+    if(this.person.name == ""){
+      alert ("Il nome della persona non può essere vuoto. \nSpecificare!");
+      return;
+    }
+    this.apiService.addPerson(this.person)
+      .subscribe(data => {
+        this.person = data;
+        this.refreshPeople();
+        this.person.name = "";
+      })
+      console.log("Sono qui");
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PersonApiService } from './service/api.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PersonApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
